85ca2d3a269a993fb8b332d31f597e62f13686c2
cnilsen <cnilsen@gmail.com>
2022-03-04 20:42:36 -0800

    formatting

--- pages/single_watershed.js
+++ pages/single_watershed.js
@@ -286,23 +286,23 @@ var makeMainSubPanel = function () {
     var mainSubPanel = ui.Panel({
         style: {
             width: '100%',
-            border: '1px solid darkgray',
+           // border: '1px solid darkgray',
             backgroundColor: 'white'
         }
     })
 
     return mainSubPanel
 };
-var makeFooter = function () {
-    var footer = ui.Panel({
-        style: {
-            width: '100%',
-            border: '1px solid darkgray',
-            backgroundColor: 'white'
-        }
-    })
-    return footer
-}
+// var makeFooter = function () {
+//     var footer = ui.Panel({
+//         style: {
+//             width: '100%',
+//             border: '1px solid darkgray',
+//             backgroundColor: 'white'
+//         }
+//     })
+//     return footer
+// }
 
 var mainPanel = helpers.makeMainPanel("Analyze a Watershed");
 
