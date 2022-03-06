5c592bc7264b22e41924c2755bd80b911afa8152
cnilsen <cnilsen@gmail.com>
2022-03-05 22:21:28 -0800

    chart area update

--- Modules/chart.js
+++ Modules/chart.js
@@ -465,7 +465,7 @@ function coc_load(layerObj, region, scale) {
     })
     .get(layerObj.layer.eeObject.bandNames()
       .get(0)));
-    var ws_area = region.area(2) 
+    var ws_area = ee.Geometry(region).area(2) 
   var conversion_factor = (layerObj.units == 'g/m² per year') ? 1e-3 : 1e-6;
  
     var total_load = total_load_per_m2.multiply(ws_area).multiply(conversion_factor) // do this for sigfigs
